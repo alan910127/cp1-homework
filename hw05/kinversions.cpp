dd57d4d80a7c0495a6666e52ff49670ed74800b5
alan910127 <alan469357@gmail.com>
2022-04-16 03:28:06 +0800

    Comments added

--- hw05/kinversions.cpp
+++ hw05/kinversions.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 using ll = long long;
 using pii = pair<int, int>;
-using fftvector = valarray<complex<double>>;
+
+using Polynomial = valarray<complex<double>>;
 const double PI = acos(-1);
 
-ll nextPow2(ll n) {
+// Find the smallest integer N such that N >= n and N = 2^k for some k is an integer.
+ll nextPowewOf2(ll n) {
     n--;
     n |= n >> 1;
     n |= n >> 2;
@@ -16,28 +18,35 @@ ll nextPow2(ll n) {
     return n + 1;
 }
 
-void fft(fftvector& vec) {
-    ll sz = vec.size();
-    if (sz <= 1) return;
+// T(n) = T(n / 2) + O(n) => T(n) = O(n log n) by Master Theorem
+void FFT(Polynomial& p) {
+    ll n = p.size();
+    if (n <= 1) return;
 
-    fftvector even{ vec[slice(0, sz / 2, 2)] };
-    fftvector odd{ vec[slice(1, sz / 2, 2)] };
+    Polynomial even{ p[slice(0, n / 2, 2)] }; // even = p[::2]
+    Polynomial odd{ p[slice(1, n / 2, 2)] };  // odd = p[1::2]
 
-    fft(even);
-    fft(odd);
+    FFT(even);
+    FFT(odd);
 
-    for (ll i = 0; i < sz / 2; ++i) {
-        complex<double> t{ polar(1.0, -2.0 * PI * i / sz) * odd[i] };
-        vec[i] = even[i] + t;
-        vec[i + sz / 2] = even[i] - t;
+    for (ll j = 0; j < n / 2; ++j) {
+        // std::polar(r, theta) = r (cos(theta) + i sin(theta))
+        // std::polar(r, theta) = r * std::exp(theta * 1i)
+        // omega = exp(2 pi i / n)  [root of unity]
+        complex<double> t{ polar(1.0, 2.0 * PI * j / n) /* <- omega^j */ * odd[j] };
+        p[j] = even[j] + t;
+        p[j + n / 2] = even[j] - t;
     }
 }
 
-void ifft(fftvector& vec) {
-    vec = vec.apply(conj);
-    fft(vec);
-    vec = vec.apply(conj);
-    vec /= vec.size();
+void IFFT(Polynomial& p) {
+    // The inverse of a DFT (discrete fourier transform) matrix is simply the DFT matrix with replacing all the omega with (1 / n) * omega^(-1)
+    // Since omega is a root of unity, omega^(-1) is the conjugate of itself
+    // Note: x * conj(omega) = conj(conj(x) * omega)
+    p = p.apply(conj);
+    FFT(p);
+    p = p.apply(conj);
+    p /= p.size();
 }
 
 int main() {
@@ -47,27 +56,36 @@ int main() {
     string str;
     cin >> str;
 
-    ll size = nextPow2(str.length() + 1) << 1;
+    // 1. We need at least str.length() points to represent p1 and p2 in point-value form (since they're both of degree str.length() - 1).
+    // 2. Multiply two polynomials of degree d will produce a polynomial of degree 2d, thus multiply by 2.
+    // 3. Find the next power of two to fit the requirement of FFT (the degree must be a power of 2).
+    ll size = nextPowewOf2(str.length() << 1);
 
-    fftvector v1(size);
-    fftvector v2(size);
+    Polynomial p1(size);
+    Polynomial p2(size);
 
     for (ll i = 0; i < str.length(); ++i) {
-        v1[i] = (str[i] - 'A') ^ 1;
-        v2[size - i - 1] = (str[i] - 'A');
+        p1[i] = (str[i] == 'A') ? 1.0 : 0.0;
+        p2[size - i - 1] = (str[i] == 'A') ? 0.0 : 1.0; // Still not understood
     }
 
-    fft(v1);
-    fft(v2);
+    // Evaluation Step
+    // Turn p1, p2 from coeffecient representation to value representation (or point-value form) by using FFT
+    FFT(p1);
+    FFT(p2);
 
+    // Multiply the value representation of the two polynomials to get the value representation of the desired p
     for (ll i = 0; i < size; ++i) {
-        v1[i] *= v2[i];
+        p1[i] *= p2[i];
     }
 
-    ifft(v1);
+    // Interpolation Step
+    // Turn the result back to the coeffecient representation by using inverse FFT
+    IFFT(p1);
 
+    // The k-inversion is actually the coeffecient of the k-th term of the result p
     for (int i = 0; i < str.size() - 1; ++i) {
-        cout << static_cast<int>(v1[i].real() + 0.25) << '\n';
+        cout << static_cast<int>(p1[i].real() + 0.25) << '\n';
     }
 
     return 0;
